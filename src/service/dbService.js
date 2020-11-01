e2e84ea70918f156010678c037263a7db780b9f0
Ryan Allen <rallen3882@gmail.com>
2020-11-01 09:59:48 -0500

    finishing up db service tests

--- src/service/dbService.js
+++ src/service/dbService.js
@@ -1,13 +1,35 @@
-async function getWallet() {
-
+async function getWallet(docClient, player) {
+    const params = {
+        TableName: process.env.TABLE_WALLETS,
+        Key: {
+            'WalletId': player
+        }
+    };
+    try {
+        const theWallet = await docClient.get(params).promise();
+        return theWallet.Item;
+    } catch (err) {
+        throw Error('db_error');
+    }
 }
 
 async function updateWallet() {
 
 }
 
-async function createWallet() {
-
+async function createWallet(docClient, player) {
+    const params = {
+        TableName: process.env.TABLE_WALLETS,
+        Item:{
+            "WalletId": player,
+            "Balance": 0
+        }
+    };
+    try {
+        await docClient.put(params).promise();
+    } catch (err) {
+        throw Error('db_error');
+    }
 }
 
 exports.getWallet = getWallet;
